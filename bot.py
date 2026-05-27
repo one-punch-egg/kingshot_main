048f33cce0287697fcb34891a1189bec747a862c
egg <146889556+one-punch-egg@users.noreply.github.com>
2026-05-27 19:12:56 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import requests
 URL = "https://kingshot.net/gift-codes"
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
 ID_MAP_FILE = "message_ids.json"
-ROLE_ID = "1469321868589793429" 
+ROLE_ID = "1469321868589793429"
 
 def get_code_data():
     try:
