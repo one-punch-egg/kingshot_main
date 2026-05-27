a5405774d7a32d0477077ae3273d3dc0890cee28
egg <146889556+one-punch-egg@users.noreply.github.com>
2026-05-27 20:33:57 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import requests
 URL = "https://kingshot.net/gift-codes"
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
 ID_MAP_FILE = "message_ids.json"
-ROLE_ID = "1469321868589793429"
+ROLE_ID = "1482141454607454308"
 
 
 def get_discord_timestamp(expiry_date_str):
